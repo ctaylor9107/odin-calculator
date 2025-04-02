460e2bfa1ac0871a049ad132c3733ce463d11d38
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-04-01 20:21:40 -0500

    Create operation function
    Created a function that will take the operator variable and use that to
    determine how to calculate the variables by calling one of the original
    functions created to do the calculations

--- calcJavascript.js
+++ calcJavascript.js
@@ -14,6 +14,21 @@ function division (number1, number2) {
     return number1/number2
 }
 
+function operate (operation, number1, number2) {
+    if (operation === "+") {
+        return addition (number1, number2)
+    }
+    else if (operation === "-") {
+        return subtraction (number1, number2)
+    }
+    else if (operation === "*") {
+        return multiplication (number1, number2)
+    }
+    else if (operation === "/") {
+        return division (number1, number2)
+    };
+};
+
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
