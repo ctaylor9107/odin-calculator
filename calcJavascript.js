4f61dc7c49870cdceb9e908129cb6c26db82e8be
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-04-01 20:12:51 -0500

    Create mathematical functions for calculator
    Created four function to do the basic operations of the calcultor:
    
    add
    subtract
    multiply
    divide

---
+++ calcJavascript.js
@@ -0,0 +1,17 @@
+function addition (number1, number2) {
+    return number1 + number2
+};
+
+function subtraction (number1, number2) {
+    return number1 - number2
+};
+
+function multiplication (number1, number2) {
+    return number1 * number2
+};
+
+function division (number1, number2) {
+    return number1/number2
+}
+
+console.log(division(16,5));
