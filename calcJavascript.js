578a142240ab9bd839b76ae37ae31ac3ac8c2bbc
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-04-02 21:45:08 -0500

    Corrected the logic of the operate function

--- calcJavascript.js
+++ calcJavascript.js
@@ -14,23 +14,24 @@ function division (number1, number2) {
     return number1/number2
 }
 
-function operate (operation, number1, number2) {
-    if (operation === "+") {
-        return addition (number1, number2)
-    }
-    else if (operation === "-") {
-        return subtraction (number1, number2)
-    }
-    else if (operation === "*") {
-        return multiplication (number1, number2)
-    }
-    else if (operation === "/") {
-        return division (number1, number2)
-    };
-};
-
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
+
+function operate (operation) {
+    if (operation === "+") {
+        return addition (firstNumber, secondNumber)
+    }
+    else if (operation === "-") {
+        return subtraction (firstNumber, secondNumber)
+    }
+    else if (operation === "*") {
+        return multiplication (firstNumber, secondNumber)
+    }
+    else if (operation === "/") {
+        return division (firstNumber, secondNumber)
+    };
+};
+
 
 console.log(division(16,5));
