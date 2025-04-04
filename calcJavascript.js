15ff0198e86c9b02f357dc1cafe5ac8358b9266f
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-04-04 14:02:00 -0500

    Create Operator Button logic and update Display
    Created the logic so that once an operator button is pressed on the
    calculator, it will capture the digits as the first number variable and
    store the operator button selected as the operator.
    
    Now that it is storing the first number variable, I was able to go back
    and update the display logic to reset itself after the next digit button
    is pressed and start the display over.

--- calcJavascript.js
+++ calcJavascript.js
@@ -17,7 +17,6 @@ function division (number1, number2) {
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
-
 let displayContent = 0;
 
 function operate (operation) {
@@ -37,30 +36,75 @@ function operate (operation) {
 
 const numButtons = document.querySelectorAll(".number");
 const displayValue = document.querySelector("#display");
+const opButtons = document.querySelectorAll(".operator");
+
+
 
 numButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        console.log(button.textContent);
-        if (displayValue.textContent == 0) {
-            displayValue.textContent = button.textContent;
-        }
-        else if (displayValue.textContent.length === 9) {
-            ""
-        }
-        else if (button.textContent == ".") {
-            if (displayValue.textContent.includes(".")) {
+            //console.log(button.textContent);
+        if (operator === "" && firstNumber === "") {
+            if (displayValue.textContent == 0) {
+                displayContent = (displayValue.textContent = button.textContent);
+            }
+            else if (displayValue.textContent.length === 9) {
                 ""
             }
+            else if (button.textContent == ".") {
+                if (displayValue.textContent.includes(".")) {
+                    ""
+                }
+                else {
+                    displayConstent = (displayValue.textContent += button.textContent);
+                };
+            }
             else {
-                displayValue.textContent += button.textContent;
-            };
+            displayContent = (displayValue.textContent += button.textContent);  
+            }
+            console.log(displayContent);
         }
-        else {
-        displayValue.textContent += button.textContent;  
+        else if (operator !== "" && firstNumber !== "") {
+            if (displayValue.textContent.length === 9) {
+                ""
+            }
+            else if (button.textContent == ".") {
+                if (displayValue.textContent.includes(".")) {
+                    ""
+                }
+                else {
+                    displayConstent = (displayValue.textContent += button.textContent);
+                };
+            }
+            else {
+            secondNumber += button.textContent;
+            displayValue.textContent = secondNumber;
+            console.log(secondNumber);
+            }
         }
     });
-
 });
 
 
-console.log(division(16,5));
+opButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (operator === "") {
+            firstNumber = displayContent;
+            displayContent = "";
+            operator = button.textContent;
+            console.log(operator);
+            console.log(firstNumber);
+        }
+        else if (operator !== "") {
+            operator = button.textContent;
+            console.log(operator);
+        }
+    })
+});
+
+
+// const equalButton = document.querySelector("#equals")
+
+// equalButton.addEventListener("click", () => {
+//     console.log(displayNumber * 2);
+// })
+
